d2667333143cf0e85f8645d59e43148b85769e13
liefn <liefn@DESKTOP-CVF8441>
2017-04-26 11:23:27 +1200

    Add project files.

---
+++ LNTrello/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LNTrello.MvcApplication" Language="C#" %>
---
+++ LNTrello/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LNTrello.Startup))]
+namespace LNTrello
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
